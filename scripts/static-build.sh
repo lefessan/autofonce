b53444ea4299722be05d36fe014fe0f80f71483f
Fabrice Le Fessant <fabrice.le_fessant@ocamlpro.com>
2023-03-15 16:55:24 +0100

    Switch to drom-share.0.9.1

--- scripts/static-build.sh
+++ scripts/static-build.sh
@@ -11,10 +11,10 @@ set -o pipefail
 
 git ls-files -z | xargs -0 tar c | \
   docker run --rm -i \
-    ocamlpro/ocaml:4.12 \
+    ocamlpro/ocaml:4.13 \
     sh -uexc \
       'tar x >&2 &&
-       sudo apk add g++ openssl-libs-static bash >&2 &&
+       sudo apk add g++ openssl-libs-static bash  >&2 &&
        opam update >&2 &&
        opam switch create . ocaml-system --deps-only --locked >&2 &&
        opam exec make LINKING_MODE=static >&2 &&
